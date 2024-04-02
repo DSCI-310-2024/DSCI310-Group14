00ec99892ed40299d236049c7d661c439e9de163
Caden Chan <cadenmchan@gmail.com>
2024-04-02 02:42:22 -0700

    Abstracted impute_split function

--- scripts/cleaning_data.py
+++ scripts/cleaning_data.py
@@ -24,11 +24,30 @@ def main(dataread,dataout,datafile1, datafile2, seed):
               'Land area (sq. km)', 'PM2.5 air pollution, mean annual exposure (micrograms per cubic meter)',
                 'Population, total','Renewable energy consumption (% of total final energy consumption)',
              'Renewable electricity output (% of total electricity output)']]
-    data1=data1.fillna(0)
-    energy_train, energy_test= train_test_split(data1, train_size=0.75)
+    energy_train, energy_test = impute_split(data1, 0, 0.75, seed)
     os.makedirs(dataout, exist_ok=True)  
     energy_test.to_csv(dataout+"/"+datafile1)  
     energy_train.to_csv(dataout+ "/"+datafile2)
+
+def impute_split(data, impute_val, train_size, rand_seed=0):  
+    """
+    Takes a data frame and creates a a training and testing split, with imputation for NA values
+
+    Parameters:
+    - data: The initial dataset (DataFrame) to be tidied
+    - impute_value: The value to replace all NA values with within the dataset
+    - train_size: A value from 0-1 that represents the % of data to be used in the training split (remainder will be used in testing split)
+    - rand_seed: A seed value to be used for randomness, allowing for reproducible results; default value is 0
+
+    Returns:
+    train: Imputed, training dataset split
+    test: Imputed, testing dataset split
+    """
+
+    numpy.random.seed(rand_seed)
+    data = data.fillna(impute_val)
+    train, test = train_test_split(data, train_size = train_size)
+    return train, test
 
 if __name__ == '__main__':
     main()
