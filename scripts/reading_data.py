d3ff401014d9bee75a19dcae60c6ee595d667721
cadenmchan <cadenmchan@gmail.com>
2024-04-11 23:21:06 -0700

    fixed import path for function_read

--- scripts/reading_data.py
+++ scripts/reading_data.py
@@ -9,7 +9,7 @@ import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-from src.functionread import reading_datain
+from src.function_read import reading_datain
 
 @click.command()
 @click.option('--url',help='URL of file you want to read in', type=str)
